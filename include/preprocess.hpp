271e6b930bdd943ecc31e16bd71308a66befd423
roboticistjoseph <jkatak73@terpmail.umd.edu>
2022-10-19 18:18:52 -0400

    [ADD] Preprocessing Class

--- include/preprocess.hpp
+++ include/preprocess.hpp
@@ -3,42 +3,88 @@
  * @author Aneesh Chodisetty (aneeshc@umd.edu)
  * @author Bhargav Kumar Soothram (bsoothra@umd.edu)
  * @author Joseph Pranadheer Reddy Katakam (jkatak@umd.edu)
- * @brief Header file for preprocess.cpp
+ * @brief Header file for Preprocessor class
  * @version 0.1
- * @date 2022-10-10
- *
+ * @date 2022-10-18
+ * 
  * @copyright Copyright (c) 2022
- *
+ * 
  */
 
 #ifndef INCLUDE_PREPROCESS_HPP_
 #define INCLUDE_PREPROCESS_HPP_
 
+// standard libraries
+#include <iostream>
 #include <string>
+
+// image processing libraries
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/mat.hpp>
 
-class Preprocess {
- public:
-  /**
-   * @brief Constructor for a preprocess object
-   *
-   */
-  Preprocess();
+/**
+ * @brief Class for Preprocessing input image data
+ * 
+ */
+class PreprocessorClass {
+ private:
+ /**
+  * @brief Variable to hold the resized frame
+  * 
+  */
+  cv::Mat resized_frame;
 
   /**
-   * @brief Destructor for a preprocess object
-   *
+   * @brief Value for resizing the image width
+   * 
    */
-  ~Preprocess();
+  float input_width;
+
+  /**
+   * @brief Value for resizing the image height
+   * 
+   */
+  float input_height;
+
+ public:
+  /**
+   * @brief Default Construct a new Preprocessor Class object
+   * 
+   */
+  PreprocessorClass();
+
+  /**
+   * @brief Custom Construct a new Preprocessor Class object
+   * 
+   * @param value_of_resize 
+   */
+  explicit PreprocessorClass(int custom_value_of_resize);
+
+  /**
+   * @brief Load the input frame
+   * 
+   * @param image_frame
+   */
+  void LoadData(cv::Mat image_frame);
 
   /**
    * @brief Function to resize/rescale the inputs as desired
-   *
-   * @param input_image
-   * @param flags
+   * 
    */
-  void preprocess(cv::Mat input_image, std::string flags);
-};
+  void Preprocess();
 
-#endif  // INCLUDE_PREPROCESS_HPP_
+  /**
+   * @brief Get the Resized Image object
+   * 
+   * @return cv::Mat 
+   */
+  cv::Mat GetResizedImage();
+
+  /**
+   * @brief Destroy the Preprocessor Class object
+   * 
+   */
+  ~PreprocessorClass();
+}
+
+#endif // INCLUDE_PREPROCESS_HPP_ 
--- src/preprocess.cpp
+++ src/preprocess.cpp
@@ -1,16 +1,53 @@
 /**
- * @file preprocess.hpp
+ * @file preprocess.cpp
  * @author Aneesh Chodisetty (aneeshc@umd.edu)
  * @author Bhargav Kumar Soothram (bsoothra@umd.edu)
  * @author Joseph Pranadheer Reddy Katakam (jkatak@umd.edu)
- * @brief Header file for model.cpp
+ * @brief Header file for Preprocessor class
  * @version 0.1
- * @date 2022-10-10
- *
+ * @date 2022-10-18
+ * 
  * @copyright Copyright (c) 2022
- *
+ * 
  */
 
+// header file for the 'Preprocessor' class
+#include "./include/preprocess.hpp"
 
-#include <preprocess.hpp>
+Preprocessor::Preprocessor() {
+    input_width = 640.0;
+    input_height = 640.0;
+}
 
+Preprocessor::Preprocessor(float custom_value_of_resize) {
+    input_width = custom_value_of_resize;
+    input_height = custom_value_of_resize;
+}
+
+void Preprocessor::LoadData(cv::Mat image_frame) {
+    resized_frame = image_frame;
+}
+
+void Preprocessor::Preprocess() {
+    int col = resized_frame.cols;
+    int row = resized_frame.rows;
+    int _max = MAX(col, row);
+
+    // formatting to uint8
+    cv::Mat resized = cv::Mat::zeros(_max, _max, CV_8UC3);
+    resized_frame.copyTo(resized(cv::Rect(0, 0, col, row)));
+
+    // resize to 640x640, normalize to [0,1[ and swap Red and Blue channels
+    cv::Mat blob;
+    cv::dnn::blobFromImage(resized_frame, blob, 1./255.,
+                            cv::Size(INPUT_WIDTH, INPUT_HEIGHT), cv::Scalar(), true, false);
+
+    resized_frame = result;
+}
+
+cv::Mat Preprocessor::GetResizedImage() {
+    return resized_frame;
+}
+
+~Preprocessor::Preprocessor() {
+}
