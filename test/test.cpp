9d85520b1c145dd2648a97997a902ce4ed3e228d
roboticistjoseph <jkatak73@terpmail.umd.edu>
2022-10-19 18:19:54 -0400

    [ADD] Tests for the Created Classes

--- test/test.cpp
+++ test/test.cpp
@@ -12,5 +12,53 @@
  */
 
 #include <gtest/gtest.h>
+#include "../include/camera.hpp"
+#include "./include/preprocess.hpp"
 
-TEST(dummy, should_pass) { EXPECT_EQ(1, 1); }
+/**
+ * @brief Test to see if the data (image) is properly being read
+ * 
+ */
+TEST(ReadData, read_data_true) {
+    // Creating instance of class
+    CameraClass camera_object;
+    // Testing the ReadData function
+    EXPECT_EQ(camera_object.ReadData("../data/human.jpg"), true);
+}
+
+/**
+ * @brief Test case 1 to check if the preprocessing of image works as expected
+ * 
+ */
+TEST(Preprocess, dimg_ata_type) {
+    // Read Image
+    auto loading_frame = cv::imread("../data/human.jpg", cv::IMREAD_COLOR);
+
+    // Creating instance of class and preprocess data
+    Preprocessor preprocessing_object;
+    preprocessing_object.LoadData(loading_frame);
+    preprocessing_object.Preprocess();
+    auto test_image = preprocessing_object.GetResizedImage();
+
+    // the datatype of preprocessed image changes from uint8 (8 bytes) to float32 (16 bytes)
+    EXPECT_EQ(static_cast<int>(test_image.type()), static_cast<int>(16));
+}
+
+/**
+ * @brief Test case 2 to check if the preprocessing of image works as expected
+ * 
+ */
+TEST(Preprocess, img_dimension) {
+    // Read Image
+    auto loading_frame = cv::imread("../data/human.jpg", cv::IMREAD_COLOR);
+
+    // Creating instance of class and preprocess data
+    Preprocessor(640) preprocessing_object;
+    preprocessing_object.LoadData();
+    preprocessing_object.Preprocess();
+    auto test_image = preprocessing_object.GetResizedImage();
+
+    // the dimension of preprocessed image is set to 640 x 640
+    EXPECT_EQ(static_cast<int>(test_image.size[0]), static_cast<int>(640));
+    EXPECT_EQ(static_cast<int>(test_image.size[1]), static_cast<int>(640));
+}
